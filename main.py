dc54b9cd71c9ce92b1c9d4746c643392c259280e
superZh <zhouhang@superZhdeMacBook-Pro.local>
2019-06-02 23:36:28 +0800

    finall job

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ import matplotlib.pyplot as plt
 
 # #作业一
 # #有向图
-G = nx.DiGraph()
-n = 0
-with open ('www.dat.gz.txt') as f:
-    for line in f:
-        n += 1
-        x, y = line.rstrip().split(' ')
-        G.add_edge(x,y)
-print(nx.info(G))
+# G = nx.DiGraph()
+# n = 0
+# with open ('www.dat.gz.txt') as f:
+#     for line in f:
+#         n += 1
+#         x, y = line.rstrip().split(' ')
+#         G.add_edge(x,y)
+# print(nx.info(G))
 # print(nx.density(G))
 
 
@@ -54,7 +54,7 @@ def plotInOutDegreeDistribution(G):
     plt.show()
 
 #print(nx.in_degree_centrality(G))
-plotInOutDegreeDistribution(G)
+#plotInOutDegreeDistribution(G)
 
 # import time
 # Ns = [i*10 for i in range(1,3)]
@@ -73,3 +73,16 @@ plotInOutDegreeDistribution(G)
 # #plt.xscale('log')
 # plt.show()
 
+import networkx as nx
+import matplotlib.pyplot as plt
+RG = nx.random_graphs.random_regular_graph(4,200)
+WS = nx.random_graphs.watts_strogatz_graph(200,4,0.3)
+print(len(WS.edges()))  
+print(len(RG.edges()))
+#生成包含200个节点、每个节点4个近邻、随机化重连概率为0.3的小世界网络
+# 首先每个节点会有4个近邻边，然后以0.3的概率去确定是否重连这条边，即保持边的一个节点不变，另一个节点随机选择
+pos = nx.spring_layout(WS)          
+#定义一个布局，此处采用了circular布局方式
+nx.draw(WS,pos,with_labels=False,node_size = 30)  
+#绘制图形
+plt.show()
